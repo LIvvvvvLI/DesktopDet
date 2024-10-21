3608cd05bff2dd2aaaf295eec3e0ab6da4301df3
LIvvvvvLI <72590627+LIvvvvvLI@users.noreply.github.com>
2024-10-21 14:50:23 +0800

    Create StringConstants.cpp

---
+++ utils/StringConstants.cpp
@@ -0,0 +1,18 @@
+#include "StringConstants.h"
+
+const QString &StringConstants::SHOW = QStringLiteral("显示");
+const QString &StringConstants::EXIT = QStringLiteral("退出");
+const QString &StringConstants::FIXED = QStringLiteral("固定");
+const QString &StringConstants::UNFIXED = QStringLiteral("取消固定");
+const QString &StringConstants::ALWAYS_STANDBY = QStringLiteral("始终待机");
+const QString &StringConstants::UNALWAYS_STANDBY = QStringLiteral("取消始终待机");
+
+const QString &TipString::FILE_LOAD_ERROR = QStringLiteral("文件加载错误");
+const QString &TipString::CHOOSE_A_EXE = QStringLiteral("选择一个可执行文件");
+const QString &TipString::EXE_FILTER = QStringLiteral("*.exe");
+const QString &TipString::CHOOSE_A_FOLDER = QStringLiteral("选择一个文件夹");
+const QString &TipString::CHOOSE_A_FILE = QStringLiteral("选择一个文件");
+const QString &TipString::INPUT_NEWNAME = QStringLiteral("输入新名称: ");
+const QString &TipString::EDIT_OR_DELETE = QStringLiteral("Yes: 修改\nNo: 删除");
+const QString &TipString::CURRENT_PATH = QStringLiteral("/");
+const QString &TipString::SUPPORT_CHINESE_IN_PATH = QStringLiteral("\"");
